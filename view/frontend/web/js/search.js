621497d3d5dca0d29a8f38a63281af389a6dd415
jags <jagadeesh@oceaniasolution.com>
2020-11-06 19:35:32 +1100

    feat(search): updated search sdk from version 0.1.9 to 2.0.0

--- view/frontend/web/js/search.js
+++ view/frontend/web/js/search.js
@@ -28,8 +28,8 @@ require(["jquery", "handlebars","UnbxdSearch"], function ($, handlebars,UnbxdSea
       if ("magento_unbxd_listingconfig" in window) {
         window.unbxdSearch = new UnbxdSearch(magento_unbxd_listingconfig);
         bindEvents();
-        renderIfMobile();
-        if (magento_unbxd_listingconfig.productType && magento_unbxd_listingconfig.productType != "SEARCH"){
+        //renderIfMobile();
+        if (magento_unbxd_listingconfig.products && magento_unbxd_listingconfig.products.productType != "SEARCH"){
           unbxdSearch.getCategoryPage();
         }
       } else {
@@ -59,57 +59,7 @@ require(["jquery", "handlebars","UnbxdSearch"], function ($, handlebars,UnbxdSea
 
   function renderIfMobile(){
     
-    if(checkMobile()) {
-      window.unbxdSearch.setFacetWidget({
-          rangeTemplate:function(ranges,selectedRanges) {
-              let ui  = ``;
-              const {
-                selectedFacetClass,
-                facetClass,
-                applyMultipleFilters
-              } = this.options.facet;
-              let selected = false;
-              ranges.forEach(range => {
-                  const {
-                    displayName,
-                    facetName,
-                    values
-                  } = range;
-                  let valueUI = ``;
-                  selected = (selectedRanges[facetName]) ? true :false;
-                  values.forEach(item =>{
-                      const {
-                        from,
-                        to
-                      } = item;
-                    const isSelected = this.isSelectedRange(facetName,item);
-                    const btnCss = (isSelected) ? `UNX-selected-facet-btn ${facetClass} ${selectedFacetClass}`:`${facetClass}`;
-                    valueUI +=[`<button class="${btnCss} UNX-range-facet UNX-change-facet" data-action="setRange" data-facet-name="${facetName}" data-start="${from.dataId}" data-end="${to.dataId}" >`,
-                        `<span class="UNX-facet-text">${from.name}  -  ${to.name}</span>`,
-                        `<span class="UNX-facet-count">(${from.count})</span>`,
-                   `</button>`].join('');
-                  });
-                  ui += [`<div class="UNX-facets-inner-wrapper">`,
-                    `<h3 class="UNX-facet-header">${displayName}</h3>`,
-                        `<div class="UNX-facets">${valueUI}</div>`,
-                  `</div>`].join('');
-              });
-              let clearBtn = ``;
-              let applyBtn = ``;
-              if(selected) {
-                  if(applyMultipleFilters) {
-                      applyBtn = `<button class="UNX-default-btn UNX-primary-btn" data-action="applyRange"> Apply</button>`;
-                  }
-                  clearBtn = `<button class="UNX-default-btn " data-action="clearRangeFacets"> clear</button>`;
-              }
-              return [`<div class="UNX-range-wrapper">`,
-                ui,
-                `<div class="UNX-price-action-row">`,
-                    applyBtn,clearBtn,
-                `<div>`,
-              `</div>`].join('')
-          }
-        });
+
         //window.unbxdSearch.bindEvents();
         window.unbxdSearch.reRender();
   

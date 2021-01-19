a139ac0acdd515eb93d1406d774d5219d8739440
jags <jagadeesh@oceaniasolution.com>
2021-01-19 10:43:57 +0530

    feat(search): Upgraded search from version 2.0.0 to 2.0.1

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,6 +3,6 @@ var config = {
     paths: {
 
         "handlebars": "https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.amd.min",
-        "UnbxdSearch": 'https://libraries.unbxdapi.com/search-sdk/v2.0.0/vanillaSearch.min'
+        "UnbxdSearch": 'https://libraries.unbxdapi.com/search-sdk/v2.0.1/vanillaSearch.min'
     }
 };
